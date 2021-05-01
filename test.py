6f900e424f9b6f713e530ffc362c825f5bff6b1e
Pimss <44709590+Pimss@users.noreply.github.com>
2021-05-01 22:21:40 +0200

    Cleaned test.py
    use of factory and slicing upscale function updated

--- test.py
+++ test.py
@@ -1,33 +1,28 @@
 from utils.prepare_images import ImageSplitter
-from Models import UpConv_7,CARN_V2
+from Models import get_model
 import torch.nn as nn
 from load_and_write import loader,DatasetNamed,DataWritter,register_fn
 
+
 import torch
 from torch.utils.data import DataLoader,Dataset
+# If cuda device is available use it
+if torch.cuda.is_available():
+	DEVICE = torch.device("cuda")
+else:
+	DEVICE = torch.device("cpu")
 
+def Uscale_splitted_frames(model, path_initial_frames, output_path,batch_size, device):
+	"""
+	function to upscale images at a given with a given model  on a specified device
+	image are splitted to use Waifu2x and run on a modest gpu
 
-if __name__ == "__main__":
-	#Parameters
-	#Path where the initial frames are located
-	path_initial_frames = "frames"
-	# Path of the model weights
-	path_weights = "model_check_points/Upconv_7/anime/noise3_scale2.0x_model.json"
-	# Path where the upscaled frames will be written
-	output_path = "results/"
-	# Batch size telling how many images you'll be processing simutanuously increase this as much as your VRAM allows it
-	batch_size=7
-
-	#Loading model and weights
-	# model = UpConv_7()
-	# model.load_pre_train_weights(json_file=path_weights)
-
-	model = CARN_V2(color_channels=3, mid_channels=64,
-				 scale=2, activation=nn.LeakyReLU(0.1),
-				 SEBlock=True, conv=nn.Conv2d,
-				 atrous=(1, 1, 1), repeat_blocks=3,
-				 single_conv_size=3, single_conv_group=1)
-	model = torch.load("trainings/model_mh_00014.pk")
+	:param: model: torch model compatible with upscaling
+	:param: path_initial_frames: path to the images to be upscaled
+	:param: output_path: path for upscale frames to be saved
+	:param: batch_size: number of image to be treated at the same time
+	:param: device: device on which the computation is performed
+	"""
 
 	#Preparing dataset and dataloader, the images
 	dataset = DatasetNamed(path_initial_frames,loader=loader,extensions=('jpg','png'))
@@ -36,21 +31,15 @@ if __name__ == "__main__":
 			   pin_memory=False, drop_last=False, timeout=0,
 			   worker_init_fn=None, prefetch_factor=2,
 			   persistent_workers=False)
-	# If cuda device is available use it
-	if torch.cuda.is_available():
-		device = torch.device("cuda")
-	else:
-		device = torch.device("cpu")
 
 	#setting model to device
 	model.to(device)
 	model = model.float() 
+	
 
 	for data,names in iter(dataloader):
 		
 		data=data.to(device)
-		
-
 		# overlapping split
 		# if input image is too large, then split it into overlapped patches 
 		# details can be found at [here](https://github.com/nagadomi/waifu2x/issues/238)
@@ -78,3 +67,26 @@ if __name__ == "__main__":
 			pass
 		#Log the images that were dealt with
 		print(names)
+
+if __name__ == "__main__":
+	#Parameters
+
+	#Path where the initial frames are located
+	path_initial_frames = "frames"
+
+	# Path of the model weights
+	path_weights ="trainings/model_mh_00014.pk"
+	# path_weights = "model_check_points/Upconv_7/anime/noise3_scale2.0x_model.json"
+	
+	# Path where the upscaled frames will be written
+	output_path = "results/"
+
+	# Batch size telling how many images you'll be processing simutanuously increase this as much as your VRAM allows it
+	batch_size=1
+	# Path to pretrained data model to be imported
+
+	#Loading model and weights
+	model = get_model('CARN_V2',path_weights)
+	# model = get_model('UpConv_7',path_weights)
+
+	Uscale_splitted_frames(model, path_initial_frames, output_path,batch_size, DEVICE)
